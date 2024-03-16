c6ff194257bdebe36cd6f783c070e2170c1799db
SNJ <142793233+SNJshaka@users.noreply.github.com>
2024-03-16 22:28:36 +0300

    Update urls.js

--- src/utilities/urls.js
+++ src/utilities/urls.js
@@ -4,9 +4,18 @@ export const options = {
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': process.env.REACT_APP_RAPID_API_HOST,
   },
-}
+};
 
-export const searchByName = name =>
-  `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/search?query=${name}`
+export const searchByName = (name) => {
+  if (!name) {
+    throw new Error('Missing search term for meals');
+  }
+  return `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/search?query=${name}`;
+};
 
-export const searchById = id => `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/${id}`
+export const searchById = (id) => {
+  if (!id) {
+    throw new Error('Missing meal ID for search');
+  }
+  return `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/${id}`;
+};
